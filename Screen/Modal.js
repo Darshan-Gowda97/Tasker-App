f470ce3316cab2d758ea1d8c5b819ac131da8727
Darshan-Gowda97 <darshangowda2016@gmail.com>
2021-02-16 22:45:48 +0530

    Testing API

--- Screen/Modal.js
+++ Screen/Modal.js
@@ -12,14 +12,25 @@ import {
   TextInput,
   SafeAreaView,
 } from 'react-native';
+import axios from 'axios';
 //  import { NavigationContainer } from '@react-navigation/native';
 
 const ModalScreen = ({open, onClose}) => {
   // const [modalVisible, setModalVisible] = useState(true);
   if (!open) return null;
 
-  const [category, setCategory] = useState('');
-  const [todo, setTodo] = useState('');
+  const category1 = 'abcd';
+  const [category, setCategory] = useState(1);
+  const [task, setTask] = useState('');
+
+  const sendRequest = ({category, task}) => {
+    const Data = {
+      id: category,
+      name: task,
+    };
+    console.log(Data);
+    return Data;
+  };
 
   return (
     <View style={styles.centeredView}>
@@ -35,19 +46,62 @@ const ModalScreen = ({open, onClose}) => {
             <Text style={styles.modalText}>STAY ACTIVE!!!</Text>
             <TextInput
               style={styles.Input}
-              placeholder="  Add Category"
+              value={category}
+              keyboardType="numeric"
+              placeholder="  Add Category ID"
               onChangeText={(val) => setCategory(val)}
             />
             <TextInput
               style={styles.Input}
               placeholder="  Add Task"
-              onChangeText={(val) => setTodo(val)}
+              value={task}
+              onChangeText={(val) => setTask(val)}
             />
-
             <View style={{flexDirection: 'row'}}>
               <TouchableOpacity
                 style={{...styles.openButton, backgroundColor: '#f8b43f'}}
-                onPress={onClose}>
+                onPress={async () => {
+                  const id = parseInt(category);
+                  const name = task;
+                  const Data1 = {name, id};
+                  console.log(Data1);
+                  const Data = JSON.stringify(Data1);
+                  console.log(Data);
+                  try {
+                    // fetch data from a url endpoint
+
+                    const data = await axios.get('/fetch_todo');
+                    console.log(data);
+                    return data;
+                  } catch (error) {
+                    console.log('error', error);
+                    // appropriately handle the error
+                  }
+                  // await axios
+                  //   .post('/add_todo', Data)
+                  //   .then(function (response) {
+                  //     console.log(response);
+                  //   })
+                  //   .catch(function (error) {
+                  //     console.log('this is the ' + error);
+                  //   });
+
+                  // fetch('add_todo', {
+                  //   method: 'POST',
+                  //   body: JSON.stringify(Data1),
+                  //   headers: {
+                  //     'Content-type': 'application/json; charset=UTF-8',
+                  //   },
+                  // })
+                  //   .then((response) => response.json())
+                  //   .then((json) => console.log(json))
+                  //   .catch(function (error) {
+                  //     console.log(
+                  //       'There has been a problem with your fetch operation: ' +
+                  //         error.message,
+                  //     );
+                  //   });
+                }}>
                 <Text style={styles.textStyle}>Submit</Text>
               </TouchableOpacity>
 
